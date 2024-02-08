60807157ce43f6b9d02ea75df19be7e6d42780f7
Tavo Cruz <madecruz2@gmail.com>
2024-02-08 12:56:10 -0800

    start movie & review documents

--- src/main/java/com/octaviocruz/movies/Movie.java
+++ src/main/java/com/octaviocruz/movies/Movie.java
@@ -1,7 +1,29 @@
 package com.octaviocruz.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
 
 @Document(collection = "movies")  //let the framework know this is a document class
+@Data // make use of lombok package, this class is a data class
+@AllArgsConstructor // that is able to create an object with a constructor with all those args
+@NoArgsConstructor // that can also make an object without any parameters
 public class Movie {
+    @Id // let framework know this is a unique type of id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseData;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
 }
